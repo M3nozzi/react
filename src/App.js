07ce06b08acbf813d600a863ca36a1aab8b20dd9
Fabio Menozzi <f.menozzi@yahoo.com>
2020-07-20 16:07:17 -0300

    handleAddRepository done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  function handleAddRepository() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: `New Repository ${Date.now()}`,
+      url: 'http://github.com/M3nozzi/reactjs',
+      techs: ['Node JS', 'React JS']
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
    
   }
 
@@ -14,13 +32,9 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+       
+        {repositories.map(repository => <li key={repository.id}>{repository.title} <button onClick={() => handleRemoveRepository(1)}>Remover</button> </li>)}
+    
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
